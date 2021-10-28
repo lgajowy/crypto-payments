3f9f5646b5744e3b1ed871738b22b083785de3da
Lukasz Gajowy <lukasz.gajowy@adverity.com>
2021-10-28 22:45:46 +0200

    Refactor: Remove obsolete code

--- src/main/scala/com/lgajowy/http/JsonFormats.scala
+++ src/main/scala/com/lgajowy/http/JsonFormats.scala
@@ -1,6 +1,5 @@
 package com.lgajowy.http
 
-import com.lgajowy.UserRegistry.ActionPerformed
 import com.lgajowy._
 import spray.json.DefaultJsonProtocol._
 import spray.json._
@@ -17,9 +16,6 @@ object JsonFormats {
     }
   }
 
-  implicit val userJsonFormat = jsonFormat3(User)
-  implicit val usersJsonFormat = jsonFormat1(Users)
-  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
   implicit val paymentResponseFormat = jsonFormat1(PaymentResponse)
   implicit val paymentRequestFormat = jsonFormat3(PaymentRequest)
   implicit val multiplePaymentsResponseFormat = jsonFormat1(MultiplePaymentsResponse)
